8902585c1a91641c307687ea036a54ce93512c4a
ikechan8370 <geyinchibuaa@gmail.com>
2023-06-02 13:20:51 +0800

    feat: add umami

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -12,4 +12,14 @@ export default defineUserConfig({
 
   // Enable it with pwa
   // shouldPrefetch: false,
+  head: [
+      ['script', {
+        async: true,
+        defer: true,
+        'data-website-id': '2ba9d94e-736d-4f32-a49e-5ea8bb58f327',
+        src: 'https://umami.geyinchi.cn/umami.js'
+      }]
+  ]
 });
+
+// <script async defer data-website-id="2ba9d94e-736d-4f32-a49e-5ea8bb58f327" src="https://umami.geyinchi.cn/umami.js"></script>
